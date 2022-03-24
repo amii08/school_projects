12fe820eeed86aedbb2e86f946f65e3ae3e3bf7a
Alex <mironenko08.03.2003@gmail.com>
2022-03-24 13:25:54 +0100

    libra_o

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='library',
+    version='0.0.1',
+    description='IDK',
+    url='',
+    author='Sasha Miron',
+    author_email='amironenka@st.swps.edu.pl',
+    license='unlicense',
+    packages=['libra'],
+    zip_safe=False
+)
